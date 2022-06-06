7e570daa668306fb5348a5abf4bcf964b439b0bc
SamirSadikovic <samir.sadikovic.bih@gmail.com>
2022-06-07 00:02:44 +0200

    Flight routing fixed

--- api/index.php
+++ api/index.php
@@ -1,10 +1,9 @@
 <?php
-echo("we are here");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once dirname(__FILE__) . '/vendor/autoload.php';
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 require_once dirname(__FILE__) . '/routes/accounts.php';
 require_once dirname(__FILE__) . '/routes/topics.php';
@@ -29,6 +28,10 @@ Flight::map('query', function($name, $defaultValue = NULL) {
     $queryParam = @$request->query->getData()[$name];
     $queryParam = $queryParam ? $queryParam : $defaultValue;
     return $queryParam;
+});
+
+Flight::route('GET /', function(){
+    Flight::redirect('../dummy.html');
 });
 
 Flight::start();
