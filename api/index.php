e0f302cfbf764fe3dac3a1c2deddf84edc50cf8e
SamirSadikovic <samir.sadikovic.bih@gmail.com>
2022-06-24 22:06:39 +0200

    Push

--- api/index.php
+++ api/index.php
@@ -31,7 +31,7 @@ Flight::register('topicService', 'TopicService');
 Flight::register('postService', 'PostService');
 Flight::register('commentService', 'CommentService');
 
-Flight::route('/*', function(){
+/* Flight::route('/*', function(){
     $path = Flight::request()->url;
     print_r($path);
     if ($path == '/login' || $path == '/')
@@ -52,7 +52,7 @@ Flight::route('/*', function(){
         return FALSE;
         }
     }
-});
+}); */
 
 Flight::route('/', function(){
     Flight::redirect('../pages/login.html');
